afe03b5295083d5c13c3e5be860a2f54b8754534
satriomandala1 <satrioareta55@gmail.com>
2025-08-19 14:39:53 +0700

    first commit

--- src/components/About.js
+++ src/components/About.js
@@ -6,12 +6,12 @@ const About = () => {
       <h2 className="section-title">About Me</h2>
       <div className="about-content">
         <div>
-          <p><strong>Full Name:</strong> Satrio Mandala Putraa  </p>
-          <p><strong>Place and date of birth:</strong> Tangerang 15 january 2007</p>
-          <p><strong>Address:</strong> tangerang, Indonesia</p>
-          <p><strong>Gender:</strong> Man</p>
+          <p><strong>I'm a web developer with experience in website development and Microsoft. I was introduced to technology in junior high school, where I learned about various computer components and their functions.</strong>  </p>
+          <p><strong>Since high school, I started helping in my cooperative. There I recorded the income and deductions of the cooperative's finances using Microsoft Excel. I also recorded items that were selling well and those that were not selling well. At that time, I also learned Microsoft Word for monthly reports.</strong></p>
+          <p><strong>and in 2024 I changed schools and there I majored in software engineering, that's where I started in the world of programming, I learned a lot of things, for example Web Development, I tried a lot to make websites with various programming languages.</strong></p>
+          {/* <p><strong>Gender:</strong> Man</p>
           <p><strong>Citizenship:</strong> Indonesia</p>
-          <p><strong>Status:</strong> siswa SMK</p>
+          <p><strong>Status:</strong> siswa SMK</p> */}
         </div>
         <div>
           <h3>Skills</h3>
